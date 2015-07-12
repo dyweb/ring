2dd600dcab4bfa0e4c6d007c023e73c7bf8499c9
xsfour <389152136@qq.com>
2015-07-12 20:30:41 +0800

    将一部分放到独立的方法

--- src/Upload.php
+++ src/Upload.php
@@ -8,7 +8,7 @@ class Upload
 {
     protected $width = 0;
     protected $height = 0;
-    protected $sizes = array();
+    protected $ratios = array();
 
     protected $maxSize = 0;
     protected $maxWidth = 0;
@@ -31,7 +31,7 @@ class Upload
         $defaultConfig = array(
             'width' => 0,
             'height' => 0,
-            'sizes' => array(0, 0),
+            'ratios' => array(0, 0),
             'maxWidth' => 0,
             'maxHeight' => 0,
             'maxSize' => 0,
@@ -67,15 +67,62 @@ class Upload
 
     public function doUpload($field)
     {
-        $this->fileInfo = $this->getFile($field);
+        $file = $this->getFile($field);
         if (is_null($this->fileInfo)) {
             $this->setError('No file selected');
-            return FALSE;
+            return false;
         }
-        //...
+
+        if (!$this->isFileUploaded($file)) {
+            return false;
+        }
+
+
+
+
+
+
         return true;
     }
 
+    protected function isFileUploaded($file)
+    {
+        if (!is_uploaded_file($file['tmp_name'])) {
+            $error = isset($file['error']) ? $file['error'] : 4;
+
+            // from CI = =
+            switch ($error) {
+                case UPLOAD_ERR_INI_SIZE:
+                    $this->setError('upload_file_exceeds_limit');
+                    break;
+                case UPLOAD_ERR_FORM_SIZE:
+                    $this->setError('upload_file_exceeds_form_limit');
+                    break;
+                case UPLOAD_ERR_PARTIAL:
+                    $this->setError('upload_file_partial');
+                    break;
+                case UPLOAD_ERR_NO_FILE:
+                    $this->setError('upload_no_file_selected');
+                    break;
+                case UPLOAD_ERR_NO_TMP_DIR:
+                    $this->setError('upload_no_temp_directory');
+                    break;
+                case UPLOAD_ERR_CANT_WRITE:
+                    $this->setError('upload_unable_to_write_file');
+                    break;
+                case UPLOAD_ERR_EXTENSION:
+                    $this->setError('upload_stopped_by_extension');
+                    break;
+                default:
+                    $this->setError('upload_no_file_selected');
+                    break;
+            }
+
+            return false;
+        }
+
+        return true;
+    }
 
     /**
      * @param $field
@@ -83,19 +130,8 @@ class Upload
      */
     protected function getFile($field)
     {
-        if (empty($field)) {
+        if (!$field) {
             return null;
-        }
-
-        if (is_array($field)) {
-            $fileInfo = array();
-            foreach ($field as $imageField) {
-                $file = $this->getFile($imageField);
-                if ($file) {
-                    $fileInfo[] = $file;
-                }
-            }
-            return empty($fileInfo) ? null : $fileInfo;
         }
 
         if (!is_string($field)) {
@@ -162,17 +198,17 @@ class Upload
         return $this;
     }
 
-    public function setSizes(array $sizes)
+    public function setRatios(array $ratios)
     {
-        $this->sizes = array();
-        if (!empty($sizes)) {
-            if (!is_array($sizes[0]) and count($sizes) === 2) {
-                $this->setWidth($sizes[0]);
-                $this->setHeight($sizes[1]);
+        $this->ratios = array();
+        if (!empty($ratios)) {
+            if (!is_array($ratios[0]) and count($ratios) === 2) {
+                $this->setWidth($ratios[0]);
+                $this->setHeight($ratios[1]);
             } else {
-                foreach ($sizes as $desc => $size) {
-                    if (@count($size) === 2) {
-                        $this->sizes[$desc] = $size;
+                foreach ($ratios as $desc => $ratio) {
+                    if (@count($ratio) === 2) {
+                        $this->ratios[$desc] = $ratio;
                     }
                 }
             }
