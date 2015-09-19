8b44d6fe4caffe7f97033714c9df251f0f256531
xsfour <389152136@qq.com>
2015-09-20 00:23:03 +0800

    add Rule to handle normal file

--- src/Dy/Ring/Rule.php
+++ src/Dy/Ring/Rule.php
@@ -11,23 +11,145 @@ namespace Dy\Ring;
 class Rule
 {
     /**
-     * @var string
-     */
-    protected $fileType = 'normal';
-
-    /**
      * max file size in Bytes
+     *
      * @var int
      */
     protected $maxSize = 0;
 
     /**
-     * @var array
+     * if overwrite when file already exists
+     *
+     * @var bool
      */
-    protected $validMimeTypes = array();
+    protected $overwrite = false;
 
+    /**
+     * if use hash filename
+     *
+     * @var bool
+     */
+    protected $hashName = true;
+
+    /**
+     * the prefix of filename
+     *
+     * @var string
+     */
+    protected $namePrefix = '';
+
+    /**
+     * the suffix of filename
+     *
+     * @var string
+     */
+    protected $nameSuffix = '';
+
+
+    /**
+     * @param array $config
+     */
     public function __construct($config = array())
     {
+        if (!empty($config)) {
+            foreach ($config as $key => $val) {
+                if (method_exists($this, 'set' . ucfirst($key))) {
+                    $this->{'set' . ucfirst($key)}($val);
+                }
+            }
+        }
 
+        if (!$this->maxSize) {
+            $this->setMaxSize(0);
+        }
+    }
+
+
+    /**
+     * @param $size
+     * @return bool
+     */
+    public function isValidSize($size)
+    {
+        return (!$this->maxSize or $size <= $this->maxSize);
+    }
+
+
+    /**
+     * @param string $fileName
+     * @return string
+     */
+    public function makeFileName($fileName)
+    {
+        if ($this->hashName) {
+            $fileName = md5($fileName . time());
+        }
+
+        $fileName = $this->namePrefix . $fileName . $this->nameSuffix;
+
+        return $fileName;
+    }
+
+
+    /**
+     * @param int $size
+     * @return $this
+     */
+    public function setMaxSize($size)
+    {
+        $size = intval($size);
+        if ($size) {
+            $this->maxSize = $size;
+        }
+
+        return $this;
+    }
+
+
+    /**
+     * @param bool $bool
+     * @return $this
+     */
+    public function setOverWrite($bool)
+    {
+        $this->overwrite = (bool)$bool;
+
+        return $this;
+    }
+
+
+    /**
+     * @param bool $bool
+     * @return $this
+     */
+    public function setHashName($bool)
+    {
+        $this->hashName = (bool)$bool;
+
+        return $this;
+    }
+
+
+    /**
+     * @param string $prefix
+     * @return $this
+     */
+    public function setNamePrefix($prefix)
+    {
+        $this->namePrefix = $prefix;
+
+        return $this;
+    }
+
+
+    /**
+     * @param string $suffix
+     * @return $this
+     */
+    public function setNameSuffix($suffix)
+    {
+        $this->nameSuffix = $suffix;
+
+        return $this;
     }
 }
