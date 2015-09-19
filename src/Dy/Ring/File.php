7a065000c570215850b2ab3ca5d25e89234d8f38
xsfour <389152136@qq.com>
2015-09-20 00:24:05 +0800

    add some method to File

--- src/Dy/Ring/File.php
+++ src/Dy/Ring/File.php
@@ -8,18 +8,89 @@
 
 namespace Dy\Ring;
 
+use Dy\Ring\Exception\FileTooLargeException;
 use Dy\Ring\FileSrc\FileSrc;
 
 class File
 {
+    /**
+     * @var FileSrc
+     */
+    protected $src;
+
+    /**
+     * @var Rule
+     */
+    protected $rule;
+
+    /**
+     * @var string
+     */
+    protected $fileName = null;
+
+    /**
+     * @var string
+     */
+    protected $fileExtension = null;
+
+    /**
+     * @param FileSrc $src
+     * @param Rule $rule
+     * @throws FileTooLargeException
+     */
     public function __construct(FileSrc $src, Rule $rule)
     {
+        $this->src = $src;
+        $this->rule = $rule;
 
+        $this->check();
     }
 
 
+    /**
+     * TODO: MIME check
+     *
+     * @throws FileTooLargeException
+     */
     public function check()
     {
-        // throw exception
+        $srcSize = $this->src->getFileSize();
+        if (!$this->rule->isValidSize($srcSize)) {
+            throw new FileTooLargeException($srcSize);
+        }
+    }
+
+
+    public function process()
+    {
+        $this->fileName = $this->rule->makeFileName($this->getFileName());
+
+        $this->getFileExtension();
+    }
+
+
+    /**
+     * @return mixed|string
+     */
+    public function getFileName()
+    {
+        if (is_null($this->fileName)) {
+            $this->fileName = pathinfo($this->src->getFileName(), PATHINFO_FILENAME);
+        }
+
+        return $this->fileName;
+    }
+
+
+    /**
+     * @return mixed|string
+     */
+    public function getFileExtension()
+    {
+        if (is_null($this->fileExtension)) {
+            $this->fileExtension = pathinfo($this->src->getFileName(), PATHINFO_EXTENSION);
+        }
+
+        return $this->fileExtension;
     }
 }
