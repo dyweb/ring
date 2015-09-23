10f862ec4e03712872f55191bcd76aabc936c3ff
xsfour <389152136@qq.com>
2015-09-23 19:51:48 +0800

    add class to save files to local file system

--- src/Dy/Ring/Backend/Backend.php
+++ src/Dy/Ring/Backend/Backend.php
@@ -10,8 +10,44 @@ namespace Dy\Ring\Backend;
 
 use Dy\Ring\File;
 
+/**
+ * Class Backend
+ * @package Dy\Ring\Backend
+ */
 abstract class Backend
 {
+    /**
+     * @var mixed
+     */
+    protected $basePath;
+
+    /**
+     * @var bool
+     */
+    protected $overWrite = false;
+
+
+    /**
+     * @param $basePath
+     * @return $this
+     * @throw \Exception
+     */
     abstract public function setBasePath($basePath);
+
     abstract public function save(File $file);
+
+
+    /**
+     * @param array $config
+     */
+    public function __construct(array $config = array())
+    {
+        if (!empty($config)) {
+            foreach ($config as $key => $val) {
+                if (method_exists($this, 'set' . ucfirst($key))) {
+                    $this->{'set' . ucfirst($key)}($val);
+                }
+            }
+        }
+    }
 }
--- src/Dy/Ring/Backend/LocalFs.php
+++ src/Dy/Ring/Backend/LocalFs.php
@@ -8,17 +8,93 @@
 
 namespace Dy\Ring\Backend;
 
+use Dy\Ring\Exception\DirectoryNotExistException;
+use Dy\Ring\Exception\DirectoryNotWritableException;
+use Dy\Ring\Exception\FileSaveFailedException;
 use Dy\Ring\File;
 
 class LocalFs extends Backend
 {
+    /**
+     * @param array $config
+     */
+    public function __construct(array $config = array())
+    {
+        parent::__construct($config);
+    }
+
+
+    /**
+     * @param $basePath
+     * @return $this
+     * @throws DirectoryNotExistException
+     * @throws DirectoryNotWritableException
+     */
     public function setBasePath($basePath)
     {
+        $this->basePath = realpath($basePath);
+        
+        if (!$this->basePath) {
+            throw new DirectoryNotExistException($basePath);
+        }
+        
+        if (!is_writable($this->basePath)) {
+            throw new DirectoryNotWritableException($this->basePath);
+        }
 
+        return $this;
     }
 
+
+    /**
+     * @param bool $bool
+     * @return $this
+     */
+    public function setOverwrite($bool)
+    {
+        $this->overWrite = (bool)$bool;
+
+        return $this;
+    }
+
+
+    /**
+     * @param File $file
+     * @throws FileSaveFailedException
+     * @throws \Exception
+     */
     public function save(File $file)
     {
+        $fullName = rtrim($this->basePath, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR . $file->getBaseName();
 
+        if (!$this->overWrite) {
+            $fileName = $file->getFileName();
+            $count = 0;
+            while (file_exists($fullName) and $count < 100) {
+                ++$count;
+                $fileName .= '(' . $count . ')';
+                $fullName = rtrim($this->basePath, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR .
+                    $fileName . '.' . $file->getFileExtension();
+            }
+
+            if (file_exists($fullName)) {
+                $fileName = $file->getFileName() . '_' . time();
+                $fullName = rtrim($this->basePath, DIRECTORY_SEPARATOR) . DIRECTORY_SEPARATOR .
+                    $fileName . '.' . $file->getFileExtension();
+            }
+
+            $file->setFileName($fileName);
+        }
+
+        try {
+            $result = $file->copyTo($fullName);
+        } catch (\Exception $e) {
+            throw $e;
+        }
+
+        if (!$result) {
+            throw new FileSaveFailedException($fullName);
+        }
     }
 }
+
