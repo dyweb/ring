b48047ce7f7d26722122bf33c70d7a415c1c9447
at15 <at15@outlook.com>
2015-10-21 15:48:23 +0800

    Add ImageTrait and RuleInterface

---
+++ src/Dy/Ring/ImageTrait.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gpl
+ * Date: 15/10/21
+ * Time: 下午3:42
+ */
+
+namespace Dy\Ring;
+
+trait ImageTrait
+{
+    /**
+     * @var int
+     */
+    protected $width = null;
+
+    /**
+     * @var int
+     */
+    protected $height = null;
+}
---
+++ src/Dy/Ring/RuleInterface.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: gpl
+ * Date: 15/10/21
+ * Time: 下午3:44
+ */
+
+namespace Dy\Ring;
+
+interface RuleInterface
+{
+
+}
