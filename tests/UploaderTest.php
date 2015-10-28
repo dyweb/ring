346e33e534dbbebb363d257672b1bfc8cfdaeb91
at15 <at15@outlook.com>
2015-10-28 22:51:44 +0800

    Add test for uploader

--- tests/UploaderTest.php
+++ tests/UploaderTest.php
@@ -9,6 +9,8 @@
 namespace Dy\Ring\Test;
 
 use Dy\Ring\Backend\LocalBackend;
+use Dy\Ring\Rule\MimeTypeRule;
+use Dy\Ring\Rule\SizeRule;
 use Dy\Ring\Source\LocalFile;
 use Dy\Ring\Uploader;
 
@@ -19,6 +21,17 @@ class UploaderTest extends \PHPUnit_Framework_TestCase
         $local = new LocalBackend("example/data");
         $source = new LocalFile("tests/images/normal.jpg");
         $uploader = new Uploader($local, $source);
+        $uploader->addRule(new SizeRule(100000));
+        $uploader->check();
+    }
+
+    public function testUploaderRuleFail()
+    {
+        $this->setExpectedException('Dy\Ring\Exception\InvalidArgumentException');
+        $local = new LocalBackend("example/data");
+        $source = new LocalFile("tests/images/normal.jpg");
+        $uploader = new Uploader($local, $source);
+        $uploader->addRule(new MimeTypeRule(array('mie/sheep')));
         $uploader->check();
     }
 }
